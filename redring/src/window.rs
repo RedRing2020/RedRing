fcf3b0284f0570d3ce8658c5bfbdab09480fe2be
RedRing <info@redring.jp>
2025-09-20 04:08:25 +0900

    初期化処理の明確化

--- redring/src/window.rs
+++ redring/src/window.rs
@@ -1,8 +1,11 @@
-use winit::{event_loop::ActiveEventLoop, window::Window, window::WindowAttributes};
+use winit::event_loop::ActiveEventLoop;
+use winit::window::{Window, WindowAttributes};
 
 pub fn create_window(event_loop: &ActiveEventLoop) -> &'static Window {
-    Box::leak(Box::new(
-        event_loop.create_window(WindowAttributes::default())
-            .expect("Failed to create window"),
-    ))
+    let attributes = WindowAttributes::default().with_title("RedRing");
+    let window = event_loop
+        .create_window(attributes)
+        .expect("Failed to create window");
+
+    Box::leak(Box::new(window))
 }
