31bfc1196f0e66492a42c845e584d8066668c84f
RedRing <info@redring.jp>
2025-10-10 01:39:09 +0900

    refactor: Clean up imports and improve code formatting in integration and unit tests

--- geo_primitives/src/unit_tests/direction2d_generic_tests.rs
+++ geo_primitives/src/unit_tests/direction2d_generic_tests.rs
@@ -20,7 +20,8 @@ mod tests {
         assert!((angle - 0.0).abs() < f64::TOLERANCE);
 
         // 角度からDirection2Dを作成
-        let dir_from_angle = Direction2D::<f64>::from_angle(geo_foundation::constants::precision::PI / 2.0);
+        let dir_from_angle =
+            Direction2D::<f64>::from_angle(geo_foundation::constants::precision::PI / 2.0);
         assert!((dir_from_angle.x() - 0.0).abs() < f64::TOLERANCE);
         assert!((dir_from_angle.y() - 1.0).abs() < f64::TOLERANCE);
     }
